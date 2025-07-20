19d1d61a02b762998a125f14cf961aa5b7a8919f
Madhuri Malvi <madhurimalvi18@gmail.com>
2025-07-20 12:26:38 +0530

    Branch A changes

--- src/p1/Class1.java
+++ src/p1/Class1.java
@@ -2,7 +2,9 @@ package p1;
 
 public class Class1 {
 	public static void main(String[] args) {
-		System.out.println("P1 C1 techno");
+
+		System.out.println("P1 C1 techno.....");
 		System.out.println("New line");
+
 	}
 }
