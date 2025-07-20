102f927dec79b21ed3554b092f9b1cb31b5700cd
Madhuri Malvi <madhurimalvi18@gmail.com>
2025-07-20 08:38:26 +0530

    Project Upload

---
+++ src/p1/Class1.java
@@ -0,0 +1,7 @@
+package p1;
+
+public class Class1 {
+	public static void main(String[] args) {
+		System.out.println("P1 C1");
+	}
+}
---
+++ src/p2/Class2.java
@@ -0,0 +1,7 @@
+package p2;
+
+public class Class2 {
+	public static void main(String[] args) {
+		System.out.println("P2 C2");
+	}
+}
