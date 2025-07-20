d735306dec908eb88b98faa19068d3f3c65bf5fe
madhurimalvi18 <madhurimalvi18@gmail.com>
2025-07-20 13:53:15 +0530

    Update Class1.java

--- src/p1/Class1.java
+++ src/p1/Class1.java
@@ -2,12 +2,9 @@ package p1;
 
 public class Class1 {
 	public static void main(String[] args) {
-<<<<<<< Updated upstream
 		System.out.println("P1 C1 techno");
 		System.out.println("New line");
-=======
 		System.out.println("P1 C1 ABC");
 		System.out.println("Hi There");
->>>>>>> Stashed changes
 	}
 }
